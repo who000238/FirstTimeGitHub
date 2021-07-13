bfc114a0841fd383f6aec11a23f4878ba3b36faf
YING SU <who000238@gmail.com>
2021-07-13 15:06:33 +0800

    try twice times

--- FirstTimeGitHub/Form1.cs
+++ FirstTimeGitHub/Form1.cs
@@ -21,5 +21,10 @@ namespace FirstTimeGitHub
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello ");
+        }
     }
 }
